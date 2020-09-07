c73715900bde08ae6d8ae8a82b86ea89b03a2a7a
JensBlack <J.Schweihoff@gmail.com>
2020-09-07 15:32:14 +0200

    deepposekit tests

--- utils/VideoAnalyzer.py
+++ utils/VideoAnalyzer.py
@@ -17,6 +17,11 @@ from utils.poser import load_deeplabcut, get_pose, find_local_peaks_new, calcula
 from utils.plotter import plot_bodyparts, plot_metadata_frame, plot_triggers_response
 from utils.configloader import VIDEO_SOURCE, OUT_DIR, ANIMALS_NUMBER
 from experiments.experiments import ExampleExperiment
+from deepposekit.models import load_model
+import numpy as np
+
+
+
 
 
 def create_dataframes(data_output):
@@ -30,7 +35,7 @@ def create_dataframes(data_output):
 
 
 print("Loading the video")
-video = cv2.VideoCapture(VIDEO_SOURCE)
+video = cv2.VideoCapture(r"D:\DeepPoseKit\Videos\video_fly.avi")
 name = os.path.split(VIDEO_SOURCE)[-1]
 video_name = "{}".format(name.split('.')[0])
 resolution = int(video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -40,10 +45,22 @@ fourcc = cv2.VideoWriter_fourcc(*'DIVX')
 output_file = os.path.join(OUT_DIR, "Analysed_" + video_name + time.strftime('%d%m%Y-%H%M%S') + ".avi")
 video_file = cv2.VideoWriter(output_file, fourcc, 30, resolution)
 
+def plot_dlc_bodyparts(image, bodyparts):
+    """
+    Plots dlc bodyparts on given image
+    adapted from plotter
+    """
+
+    for bp in bodyparts:
+        center = tuple(bp.astype(int))
+        cv2.circle(image, center=center, radius=3, color=(255, 0, 0), thickness=2)
+    return image
 
 def start_videoanalyser():
-    print("Starting DeepLabCut")
-    config, sess, inputs, outputs = load_deeplabcut()
+    print("Starting DeepPoseKit")
+    path_model = r"D:\DeepPoseKit\models\Fly\best_model_densenet.h5"
+    model = load_model(path_model)
+
 
     experiment_enabled = False
     video_output = True
@@ -60,17 +77,21 @@ def start_videoanalyser():
     while video.isOpened():
         ret, frame = video.read()
         if ret:
-            scmap, locref, pose = get_pose(frame, config, sess, inputs, outputs)
-            peaks = find_local_peaks_new(scmap, locref, ANIMALS_NUMBER, config)
-            skeletons = calculate_skeletons(peaks, ANIMALS_NUMBER)
-            if skeletons:
-                for skeleton in skeletons:
-                    if experiment_enabled:
-                        result, response = experiment.check_skeleton(frame, skeleton)
-                        plot_triggers_response(frame, response)
-                out_frame = plot_bodyparts(frame, skeletons)
-            else:
-                out_frame = frame
+            frame = frame = frame[..., 1][..., None]
+            predictions = model.predict(frame, verbose = 1)
+            # scmap, locref, pose = get_pose(frame, config, sess, inputs, outputs)
+            # peaks = find_local_peaks_new(scmap, locref, ANIMALS_NUMBER, config)
+            # skeletons = calculate_skeletons(peaks, ANIMALS_NUMBER)
+            # if skeletons:
+            #     for skeleton in skeletons:
+            #         if experiment_enabled:
+            #             result, response = experiment.check_skeleton(frame, skeleton)
+            #             plot_triggers_response(frame, response)
+            #     out_frame = plot_bodyparts(frame, skeletons)
+            # else:
+            x,y,confidence = np.split(predictions,3,-1)
+            plot_dlc_bodyparts(predictions[:,:3])
+            out_frame = frame
             cv2.imshow('stream', out_frame)
             if video_output:
                 video_file.write(out_frame)
