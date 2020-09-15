42b2215faf03aabb9a5ddd18b9d76698452a4b5e
JensBlack <J.Schweihoff@gmail.com>
2020-09-15 19:10:03 +0200

    updated configloader.py

--- utils/configloader.py
+++ utils/configloader.py
@@ -51,6 +51,7 @@ VIDEO = dsc_config['Video'].getboolean('VIDEO')
 
 
 # experiment
+EXP_ORIGIN = dsc_config['Experiment'].get('EXP_NAME')
 EXP_NAME = dsc_config['Experiment'].get('EXP_NAME')
 RECORD_EXP = dsc_config['Experiment'].getboolean('RECORD_EXP')
 
