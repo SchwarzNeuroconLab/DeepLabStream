1250a869842940fca34a450dac7abef9d7fa3e47
JensBlack <J.Schweihoff@gmail.com>
2020-11-25 17:29:20 +0100

    reset to multiple networks

--- utils/configloader.py
+++ utils/configloader.py
@@ -27,17 +27,12 @@ with open(cfg_path) as cfg_file:
 adv_cfg_path = os.path.join(os.path.dirname(__file__), 'advanced_settings.ini')
 with open(adv_cfg_path) as adv_cfg_file:
     adv_dsc_config.read_file(adv_cfg_file)
-# DeepLabCut
-deeplabcut_config = dict(dsc_config.items('DeepLabCut'))
 
 #poseestimation
 MODEL_ORIGIN = dsc_config['Pose Estimation'].get('MODEL_ORIGIN')
 MODEL_PATH = dsc_config['Pose Estimation'].get('MODEL_PATH')
 MODEL_NAME = dsc_config['Pose Estimation'].get('MODEL_NAME')
 ALL_BODYPARTS = tuple(part for part in dsc_config['Pose Estimation'].get('ALL_BODYPARTS').split(','))
-
-
-
 
 # Streaming items
 try:
@@ -46,7 +41,7 @@ except ValueError:
     print('Incorrect resolution in config!\n'
           'Using default value "RESOLUTION = 848, 480"')
     RESOLUTION = (848, 480)
-MODEL = dsc_config['Streaming'].get('MODEL')
+
 FRAMERATE = dsc_config['Streaming'].getint('FRAMERATE')
 OUT_DIR = dsc_config['Streaming'].get('OUTPUT_DIRECTORY')
 CAMERA_SOURCE = dsc_config['Streaming'].get('CAMERA_SOURCE')
@@ -64,15 +59,6 @@ EXP_NAME = dsc_config['Experiment'].get('EXP_NAME')
 RECORD_EXP = dsc_config['Experiment'].getboolean('RECORD_EXP')
 
 START_TIME = time.time()
-
-#Classification
-PATH_TO_CLASSIFIER = dsc_config['Classification'].get('PATH_TO_CLASSIFIER')
-PIXPERMM = dsc_config['Classification'].getfloat('PIXPERMM')
-ALL_BODYPARTS = [str(part).strip() for part in dsc_config['Classification'].get('ALL_BODYPARTS').split(',')]
-THRESHOLD = dsc_config['Classification'].getfloat('THRESHOLD')
-POOL_SIZE = dsc_config['Classification'].getint('POOL_SIZE')
-TIME_WINDOW = dsc_config['Classification'].getint('TIME_WINDOW')
-
 
 """advanced settings"""
 STREAMS = [str(part).strip() for part in adv_dsc_config['Streaming'].get('STREAMS').split(',')]
