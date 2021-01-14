85e0d2b6328909fbdc2dfa282d76c971da9fb3e7
JensBlack <J.Schweihoff@gmail.com>
2021-01-14 11:17:04 +0100

    updated handling missing data functionality

--- utils/configloader.py
+++ utils/configloader.py
@@ -73,6 +73,7 @@ ANIMALS_NUMBER = adv_dsc_config['Streaming'].getint('ANIMALS_NUMBER') if adv_dsc
     'ANIMALS_NUMBER') is not None else 1
 
 REPEAT_VIDEO = adv_dsc_config['Video'].getboolean('REPEAT_VIDEO')
+RECORD_RAW = adv_dsc_config['Video'].getboolean('RECORD_RAW')
 CROP = adv_dsc_config['Streaming'].getboolean('CROP')
 CROP_X = [int(str(part).strip()) for part in adv_dsc_config['Streaming'].get('CROP_X').split(',')]
 CROP_Y = [int(str(part).strip()) for part in adv_dsc_config['Streaming'].get('CROP_Y').split(',')]
