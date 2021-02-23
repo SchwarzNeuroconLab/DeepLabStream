82dbe96a13245355de2b29badd6f616e59f4605c
JensBlack <J.Schweihoff@gmail.com>
2021-02-23 11:47:21 +0100

    cleaned requirements.txt added text to webcam.py

--- utils/webcam.py
+++ utils/webcam.py
@@ -1,3 +1,12 @@
+"""
+DeepLabStream
+© J.Schweihoff, M. Loshakov
+University Bonn Medical Faculty, Germany
+https://github.com/SchwarzNeuroconLab/DeepLabStream
+Licensed under GNU General Public License v3.0
+"""
+
+
 from utils.generic import GenericManager, MissingFrameError
 import time
 import base64
